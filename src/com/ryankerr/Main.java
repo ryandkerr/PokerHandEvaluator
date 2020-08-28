ee754b80c1c48d604fbdbd54ea3f32445700751d
Ryan Kerr <ryandanielkerr@gmail.com>
2020-08-28 16:46:15 -0700

    Take user input using scanner

--- src/com/ryankerr/Main.java
+++ src/com/ryankerr/Main.java
@@ -1,71 +1,16 @@
 package com.ryankerr;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
-    /*
-    evaluateHands(hand h1, hand h2, table cards);
-        - Generates all possible combinations of table cards
-        - for each hand, generate best hand from hand + table cards
-        - evaluate which hand wins & keep track of winner
-        - return list of probability each hand wins
-
-    General structure:
-    tableCardCombinations = generateHands(handCards, tableCards)
-    for tableCards in tableCardCombinations:
-      bestHand
-      for hand in handCards:
-        best = findBestHand(hand, tableCards)
-        if best > bestHand:
-          bestHand = best
-      keep track of which hand won
-
-    do math to get percent that each hand won
-    return list of percentages
-
-    Methods:
-    generateHands(handCards : list[card], tableCards : list[card]):  list[list[card]]
-      deck = new Deck
-      for card in handCards + tableCards:
-         remove card from deck
-      return combinations(deck)
-
-    findBestHand(hand : list[card], tableCards : list[card]): list[cards]
-      fiveCardCombinations = generate combinations of 5-card hands
-      bestHand
-      for hand in fiveCardCombinations:
-        if hand is best:  TODO, compare hands
-          bestHand = best
-
-      return best hand
-
-    isBetterHand(hand1 : list[card], hand2 : list[card]): byte (1 if true, 0 if tied, -1 if false)
-      evaluation1 = Evaluation(hand1)
-      evaluation2 = Evaluation(hand2)
-      if evaluation1 == evaluation2:
-        return 0
-      else if evaluation1 > evaluation 2:
-        return 1
-      else:
-        return -1
-
-
-    Abstractions:
-    - Card (suit, value)
-    - Deck (list of cards, add, remove)
-    - five-card hand (list of cards, evaluation)
-    */
-
-    // argument format: java com.ryankerr.Main [hand1] [hand2] [handN] -t [tableCards]
-    // where hand cards are like: "ASKD"
-    // -t signifies that the next cards are table cards
-    // table cards are like: "JSQH9D"
-    ArrayList<ArrayList<Card>> hands = parseHands(args);
-    ArrayList<Card> tableCards = parseTableCards(args);
+    ArrayList<ArrayList<Card>> hands = getHandsFromUser();
+    ArrayList<Card> tableCards = getTableCardsFromUser();
     float[] winProbabilities = HandEvaluator.calculateWinProbability(hands, tableCards);
 
+    System.out.println("\nWin Probabilities:\n");
     for (int i = 0; i < winProbabilities.length; i++) {
       String hand = "";
       if (i == winProbabilities.length - 1) {
@@ -77,27 +22,28 @@ public class Main {
     }
   }
 
-  // returns the list of 2-card hands from the command-line arguments
-  private static ArrayList<ArrayList<Card>> parseHands(String[] args) {
+  private static ArrayList<ArrayList<Card>> getHandsFromUser() {
     ArrayList<ArrayList<Card>> hands = new ArrayList<ArrayList<Card>>();
-    for (String arg : args) {
-      if (arg.equals("-t")) {
+    int handNum = 1;
+    while (true) {
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("Hand " + handNum + ": ");
+      String handString = scanner.nextLine();
+      if (handString.length() == 0) {
         break;
       }
-      ArrayList<Card> hand = cardStringToCardList(arg);
+      ArrayList<Card> hand = cardStringToCardList(handString);
       hands.add(hand);
+      handNum++;
     }
     return hands;
   }
 
-  private static ArrayList<Card> parseTableCards(String[] args) {
+  private static ArrayList<Card> getTableCardsFromUser() {
     ArrayList<Card> tableCards = new ArrayList<>();
-    for (int i = 0; i < args.length; i++) {
-      if (args[i].equals("-t") && i < args.length - 1) {
-        tableCards = cardStringToCardList(args[i+1]);
-      }
-    }
-    return tableCards;
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Table Cards: ");
+    return cardStringToCardList(scanner.nextLine());
   }
 
   // takes a string like "AHAD" and returns a list with two cards, the Ace of Hearts and Ace of
